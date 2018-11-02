a817cf6a745b9b05ae6a8d06cd0471184c311bbd
dtan_yh85 <darren243@hotmail.com>
2018-11-02 14:34:47 +0800

    Video 47 - Adding A Touch Of Audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource thrusterSound;
     [SerializeField] int thrustSpeed = 100;
     [SerializeField] int rotationSpeed = 10;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        thrusterSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -21,10 +23,19 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space))    
+        if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime); // Vector3.up is in the Y direction, .AddRelativeForce add force which is always relative to the object position
+            if (!thrusterSound.isPlaying)   // so it doesn't layer
+            {
+                thrusterSound.Play();
+            }
         }
+        else
+        {
+            thrusterSound.Stop();
+        }
+
         if (Input.GetKey(KeyCode.A) && (Input.GetKey(KeyCode.D) == false)) // Rotate Left
         {
             transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);  // Vector3.forward is rotation in the Z direction
