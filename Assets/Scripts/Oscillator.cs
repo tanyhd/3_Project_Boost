1e0cbf77d2d29219f8b4c2693bf226961c742ea1
dtan_yh85 <darren243@hotmail.com>
2018-11-03 17:11:36 +0800

    Video 61 - Mathf.Sin() For Movement Cycles

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
 
-    [SerializeField] Vector3 movementVector;
-    [Range (0, 1)] [SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved.
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f; // Time taken to complete one full cycle, default is 2 second
+
+    float movementFactor; // 0 for not moved, 1 for fully moved.
 
     Vector3 startingPos;
 
@@ -17,7 +19,13 @@ public class Oscillator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        float cycles = Time.time / period;  // Number of cycles which it have gone through as the game progresses 
+
+        const float tau = Mathf.PI * 2f;    // 360 deg
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to 1
+
+        movementFactor = (rawSinWave / 2f) + 0.5f;
         Vector3 offset = movementVector * movementFactor;
-        transform.position = startingPos + offset;
+        transform.position = startingPos + offset; 
 	}
 }
